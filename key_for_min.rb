3fef052157ba83e52b2d324c71046d4df578280c
MarkBezsonov <markbezsonov@gmail.com>
2021-02-04 02:17:05 -0600

    commit tree 3564beae4e8c1f770eae5a49ae03f5a680322269

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,18 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+name_hash = {"blake" => 500, "ashley" => 2, "adam" => 1}
 
-end
+def key_for_min_value(name_hash)
+    lowest_key = nil 
+    lowest_value = nil 
+    name_hash.each do |x, y|
+      if lowest_value == nil || y < lowest_value
+        lowest_value = y 
+        lowest_key = x 
+      end 
+    end 
+    lowest_key
+  end 
+  
+  key_for_min_value(name_hash)
